4bc462d61219e5232ea129adb5122be9b22fe72e
omarfarukee <umarfaruque1920@gmail.com>
2022-11-08 04:46:53 +0600

    six card done

--- src/page/HomeFoodCard/HomeFoodCard.js
+++ src/page/HomeFoodCard/HomeFoodCard.js
@@ -2,22 +2,24 @@ import React, {useState, useEffect} from 'react';
 import MapFoodCard from './MapFoodCard/MapFoodCard';
 
 const HomeFoodCard = () => {
-    const [cards, setCards] = useState({})
+    const [cards, setCards] = useState([])
 
     useEffect(() =>{
         fetch('Food.json')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => setCards(data))
     },[])
     return (
         <div>
-{/*             
-            {
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 p-4 ml-10'>
+                  {
                 cards.map(card => <MapFoodCard
                 key={card.id}
                 card={card}
                 ></MapFoodCard>)
-            } */}
+            }
+            </div>
+          
         </div>
     );
 };
--- src/page/HomeFoodCard/MapFoodCard/MapFoodCard.js
+++ src/page/HomeFoodCard/MapFoodCard/MapFoodCard.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-const MapFoodCard = ({card}) => {
-    console.log(card)
+const MapFoodCard = ({ card }) => {
+    const { id, img, name, price, details } = card
     return (
         <div>
-            
+            <div className="card card-compact w-96  bg-black shadow-xl mb-3">
+                <figure><img className='h-60 w-full' src={img} alt="" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{name}</h2>
+                    <p>{details.slice(400)}...</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">More Details</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
